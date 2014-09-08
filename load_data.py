1b4558685417f2f1747bc3699f0a675ebceec5f0
Alexey Bezhan <al@fueledbylemons.com>
2014-09-08 22:40:47 +0100

    Remove API methods and scrape timetable pages instead
    There doesn't seem to be a foolproof way to connect together
    route's first and last stops with destination references
    from the API. Which means there's no way to find which
    service points chain does a busTime belong to

--- load_data.py
+++ load_data.py
@@ -2,9 +2,11 @@ import datetime
 import json
 import os
 import sys
+from time import sleep
 from hashlib import md5
 
 import requests
+from lxml.html import fromstring
 
 
 CACHE_PATH = 'data/'
@@ -30,10 +32,32 @@ class BusTracker(object):
         time_string = datetime.datetime.utcnow().strftime("%Y%m%d%H")
         return md5(self.api_key + time_string).hexdigest()
 
+    def get_all(self):
+        routes = []
+        for service in self.services:
+            routes.extend(self.get(service))
+
+        return routes
+
     def get(self, service):
         routes = self.get_routes(service)
-        self.services[service]['routes'] = routes
-        return self.services[service]
+        timetables, bus_routes = [], []
+        for route in routes:
+            stops = filter(lambda x: 'code' in x, route['points'])
+            start, stop = stops[0]['code'], stops[-1]['code']
+            times = self.get_full_timetable(service, start, stop)
+
+            # Some routes have duplicates: same start and destination stops
+            # but slightly different service points along the way.
+            # This leads to multiple buses starting at the same time.
+            # Since there's no easy way to separate timetables between these subroutes,
+            # we'll just drop routes with times we've seen before
+            if times not in timetables:
+                route['buses'] = map(rel_time, times)
+                timetables.append(times)
+                bus_routes.append(route)
+
+        return bus_routes
 
     def get_services(self):
         services = {}
@@ -43,18 +67,6 @@ class BusTracker(object):
 
         return services
 
-    def get_stops(self):
-        stops_data = get_cached('stops.json', self.request_stops)
-        return stops_data['busStops']
-
-    def get_destinations(self):
-        destinations = {}
-        dest_data = get_cached('destinations.json', self.request_destinations)
-        for dest in dest_data['dests']:
-            destinations[dest['ref']] = dest
-
-        return destinations
-
     def get_routes(self, service):
         return get_cached(
             "%s_route.json" % (service),
@@ -62,62 +74,60 @@ class BusTracker(object):
             service
         )
 
-    def get_timetables(self, service, stop, destination=None, day=0, time=None):
-        # We care about the timetable weekday, not offset from today
-        # This is somewhat wrong, since day to day times might be different,
-        # but that doesn't really matter much in this case
-        weekday = (datetime.date.today() + datetime.timedelta(days=day)).weekday()
+    def get_full_timetable(self, service, start, stop):
+        time = 0
+        times = []
+        # Would you look at that, a while loop in python
+        while (time < 2400):
+            timetable = self.get_timetables(service, start, stop, "%04d" % time)
+            if timetable:
+                times.extend(timetable)
+                if int(timetable[0]['start']) < time:
+                    drop_cache("_".join([service, start, stop, "%04d" % time, 'timetable.html']))
+                else:
+                    time = int(timetable[-1]['start'])
+            else:
+                time += 200
 
-        filename = "_".join(map(str, [
-            service,
-            "timetable",
-            stop,
-            destination,
-            weekday,
-            time
-        ]))
+        return times
 
-        timetable = get_cached(
-            "%s.json" % filename,
+    def get_timetables(self, *args):
+        response = get_cached(
+            "_".join(args + ("timetable.html",)),
             self.request_timetables,
-            service, stop, destination, day, time
+            *args
         )
 
-        return timetable
+        doc = fromstring(response)
+        rows = [
+            elem.xpath('./td//text()') for elem in doc.xpath('//table[@class="timetable"][2]/tr')
+        ]
+        if not rows:
+            return rows
+
+        start_times, stop_times = rows[0][1:], rows[-1][1:]
+
+        return [{"start": s, "stop": e} for s, e in zip(start_times, stop_times)]
 
     def request_services(self):
         return self.call_api('getServices')
-
-    def request_stops(self):
-        return self.call_api('getBusStops')
-
-    def request_destinations(self):
-        return self.call_api('getDests')
-
-    def request_service_points(self, service):
-        service_ref = self.services[service]['ref']
-        return self.call_api('getServicePoints', {'ref': service_ref})
 
     def request_routes(self, service):
         base_url = 'http://lothianbuses.com/tt/parse.php?service='
         return requests.get(base_url + service).json()
 
-    def request_timetables(self, service, stop, destination=None, day=0, time=None):
-        service_ref = self.services[service]['ref']
-        return self.call_api('getBusTimes', {
-            'stopId': stop,
-            'refService': service_ref,
-            'refDest': destination,
-            'time': time,
-            'day': day,
-            'nb': 10,
-        })
+    def request_timetables(self, service, start, stop, time=None):
+        base_url = 'http://lothianbuses.com/plan-a-journey/timetables/'
 
-    def request_journey(self, stop, journey):
-        return self.call_api('getJourneyTimes', {
-            'stopId': stop,
-            'journeyId': journey,
-        })
+        return requests.get(base_url + service, params={
+            's': start,
+            'e': stop,
+            'b': time,
+            'sid': service,
+            'p': 120,  # time range in minutes
+            'd[]': 0,  # monday to friday
+            'self': '',
+        }).text
 
     def call_api(self, function, params=None):
         if params is None:
@@ -138,18 +148,32 @@ def is_cached(filename):
 
 
 def save_data(data, filename):
-    with open(CACHE_PATH + filename, 'w') as file:
-        json.dump(data, file)
+    with open(CACHE_PATH + filename, 'w') as f:
+        if filename.endswith('.json'):
+            json.dump(data, f)
+        else:
+            f.write(data)
 
 
 def read_data(filename):
-    with open(CACHE_PATH + filename, 'r') as file:
-        return json.load(file)
+    with open(CACHE_PATH + filename, 'r') as f:
+        if filename.endswith('.json'):
+            return json.load(f)
+        else:
+            return f.read()
+
+
+def drop_cache(filename):
+    os.remove(CACHE_PATH + filename)
 
 
 def get_cached(filename, request_method, *args, **kwargs):
     cache_filename = filename
     if not is_cached(cache_filename):
+        # plan-a-journey pages start returning wrong times if
+        # there requests are coming without delay.
+        # Also, it's good to be nice
+        sleep(5)
         data = request_method(*args, **kwargs)
         save_data(data, cache_filename)
     else:
@@ -158,6 +182,10 @@ def get_cached(filename, request_method, *args, **kwargs):
     return data
 
 
+def rel_time(bus_time):
+    return dict([(k, ((int(v) / 100) * 60 + int(v) % 100)) for k, v in bus_time.items()])
+
+
 if __name__ == '__main__':
     api_key = sys.argv[1]
-    print BusTracker(api_key=api_key).get("38")
+    save_data(BusTracker(api_key=api_key).get_all(), 'all.json')
