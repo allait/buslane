32aef1080b0ceb1968dc4c970b3e8addcea6ef64
Alexey Bezhan <al@fueledbylemons.com>
2014-09-11 20:48:53 +0100

    Add controls form, load services list on start

--- js/main.js
+++ js/main.js
@@ -10,103 +10,149 @@ var map = new google.maps.Map(d3.select("#map").node(), {
   draggable: false,
 });
 
-d3.json("data/" + window.location.hash.substring(1) + ".json", function (data) {
-  var overlay = new google.maps.OverlayView();
+var drawOverlay = function (data, startHour, timelapseMinutes) {
 
-  var startHour = 10;
-  var timelapseMinutes = 5;
-
-  overlay.onAdd = function () {
-    var adjTime = function (time) {
-      var total = timelapseMinutes * 60 * 1000;
-      return (time / (24 * 60)) * total;
-    };
-
-    var timeOffset = function (time) {
-      var relativeTime = (Math.floor(time / 100) * 60 + (time % 100));
-      var otime = relativeTime - (startHour * 60);
-      if (otime < 0) {
-        otime += 24 * 60;  // move time to after midnight
-      }
-      return otime;
-    };
-
-    var layer = d3.select(this.getPanes().overlayLayer)
-                  .append("div").attr("class", "overlay")
-                    .append("svg:svg");
-
-    overlay.draw = function () {
-      var projection = this.getProjection();
-
-      var transform = function (d) {
-        d = new google.maps.LatLng(d.lat, d.lng);
-        d = projection.fromLatLngToDivPixel(d);
-        return d;
-      };
-
-      var time = layer.append('svg:text')
-                      .attr('class', 'clock')
-                      .attr('x', '50%').attr('y', '100px').attr('font-size', "64px")
-                      .text("23:59");
-
-      time.transition().duration(adjTime(24 * 60))
-          .ease("linear")
-          .tween("text", function (d) {
-            var interpolate = d3.interpolate(0, 24 * 60);
-            var format = d3.format("02d");
-            return function (t) {
-              var time = interpolate(t);
-              var hours = (Math.floor(time / 60) + startHour) % 24;
-              var minutes = Math.floor(time % 60);
-              this.textContent = format(hours) + ':' + format(minutes);
-            };
-          });
-
-      var routes = layer.selectAll().data(data).enter()
-                        .append("svg:g").attr("class", "route");
-
-      var line = d3.svg.line()
-          .x(function (d) { return transform(d).x; })
-          .y(function (d) { return transform(d).y; })
-          .interpolate("linear");
-
-      var route_line = routes.selectAll()
-          .data(function (d) { return [d.points]; }).enter()
-          .append("svg:path").attr("d", line);
-
-      var colors = d3.scale.category20();
-      var buses = routes.selectAll().data(function (d, i) {
-        d.buses.forEach(function (v) { v['route'] = i; });
-        return d.buses;
-      }).enter()
-          .append("svg:circle")
-          .attr("r", 4)
-          .attr("fill", function (d, i) { return colors(d.route); })
-          .attr("class", "bus")
-          .attr("transform", function (d) {
-            return "translate(" + transform(d3.select(this.parentNode).datum().points[0]) + ")";
-          });
-
-      buses.transition()
-        .ease("linear")
-        .delay(function (d, i) {
-          return adjTime(timeOffset(d.start));
-        })
-        .duration(function (d, i) {
-          return adjTime(timeOffset(d.stop) - timeOffset(d.start));
-        })
-        .attrTween("transform", function (d, i, a) {
-          var path = d3.select(this.parentNode).select('path')[0][0];
-          var l = path.getTotalLength();
-          return function (t) {
-            var p = path.getPointAtLength(t * l);
-            return "translate(" + p.x + "," + p.y + ")";
-          };
-        })
-        .transition().duration(2000).style("opacity", 0).remove();
-    };
+  var adjTime = function (time) {
+    var total = timelapseMinutes * 60 * 1000;
+    return (time / (24 * 60)) * total;
   };
 
-  overlay.setMap(map);
-});
+  var timeOffset = function (time) {
+    var relativeTime = (Math.floor(time / 100) * 60 + (time % 100));
+    var otime = relativeTime - (startHour * 60);
+    if (otime < 0) {
+      otime += 24 * 60;  // move time to after midnight
+    }
+    return otime;
+  };
 
+  return function () {
+
+    d3.select("svg").remove();
+
+    var layer = d3.select("div.overlay").append("svg:svg");
+    var projection = this.getProjection();
+
+    var transform = function (d) {
+      d = new google.maps.LatLng(d.lat, d.lng);
+      d = projection.fromLatLngToDivPixel(d);
+      return d;
+    };
+
+    var time = layer.append('svg:text')
+                    .attr('class', 'clock')
+                    .attr('x', '80%').attr('y', '100px').attr('font-size', "64px")
+                    .text("23:59");
+
+    time.transition().duration(adjTime(24 * 60))
+        .ease("linear")
+        .tween("text", function (d) {
+          var interpolate = d3.interpolate(0, 24 * 60);
+          var format = d3.format("02d");
+          return function (t) {
+            var time = interpolate(t);
+            var hours = (Math.floor(time / 60) + startHour) % 24;
+            var minutes = Math.floor(time % 60);
+            this.textContent = format(hours) + ':' + format(minutes);
+          };
+        });
+
+    var routes = layer.selectAll().data(data).enter()
+                      .append("svg:g").attr("class", "route");
+
+    var line = d3.svg.line()
+        .x(function (d) { return transform(d).x; })
+        .y(function (d) { return transform(d).y; })
+        .interpolate("linear");
+
+    var route_line = routes.selectAll()
+        .data(function (d) { return [d.points]; }).enter()
+        .append("svg:path").attr("d", line);
+
+    var colors = d3.scale.category20();
+    var buses = routes.selectAll().data(function (d, i) {
+      d.buses.forEach(function (v) { v['route'] = i; });
+      return d.buses;
+    }).enter().append("svg:circle");
+
+    buses.transition()
+      .ease("linear")
+      .delay(function (d, i) {
+        return adjTime(timeOffset(d.start));
+      })
+      .each("start", function () {
+        d3.select(this)
+          .attr("r", 4)
+          .attr("fill", function (d, i) { return colors(d.route); })
+          .attr("class", "bus");
+      })
+      .duration(function (d, i) {
+        return adjTime(timeOffset(d.stop) - timeOffset(d.start));
+      })
+      .attrTween("transform", function (d, i, a) {
+        var path = d3.select(this.parentNode).select('path')[0][0];
+        var l = path.getTotalLength();
+        return function (t) {
+          var p = path.getPointAtLength(t * l);
+          return "translate(" + p.x + "," + p.y + ")";
+        };
+      })
+      .transition().duration(2000).style("opacity", 0).remove();
+  };
+};
+
+var loadData = function (params) {
+  var service = params['service'],
+      startHour = parseInt(params['start'], 10) || 0,
+      timelapseMinutes = parseInt(params['duration'], 10) || 0;
+
+  var overlay = new google.maps.OverlayView();
+
+  d3.json("data/" + service + ".json", function (data) {
+    overlay.onAdd = function () {
+      var layer = d3.select(this.getPanes().overlayLayer).selectAll("div.overlay")
+                    .data([1]).enter().append("div").attr("class", "overlay");
+
+    };
+    overlay.draw = drawOverlay(data, startHour, timelapseMinutes);
+
+    overlay.setMap(map);
+  });
+
+  return overlay;
+};
+
+var initialize = function () {
+  var start = 18;
+  var duration = 2;
+  var params = $.deparam.fragment() || {service: "1", start: 10, duration: 10};
+
+  $.getJSON('data/services.json').done(function (data) {
+    d3.select('#service').selectAll().data(data)
+      .enter().append("option")
+      .attr("value", function (d) {
+        return d.mnemo;
+      })
+      .text(function (d) {
+        return d.mnemo;
+      });
+
+    $.each(params, function (k, v) {
+      $('select[name="' + k + '"]').val(v);
+    });
+  });
+
+  loadData(params);
+
+  $('select').on('change', function () {
+    var params = {};
+    $.each($('#controls').serializeArray(), function (_, i) {
+      params[i.name] = i.value;
+    });
+    $.bbq.pushState($('#controls').serialize(), 2);
+
+    loadData(params);
+  });
+};
+
+$(document).ready(initialize);
